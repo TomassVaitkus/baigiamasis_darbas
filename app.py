95220b4ac65dbddc27d5c903805d370800089ea2
Shliosen <tv.vaitkus@gmail.com>
2023-05-30 18:06:39 +0300

    papildziau atliktais darbais

--- app.py
+++ app.py
@@ -1,110 +1,241 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-from form import OrderForm
+from flask import Flask, render_template, request, redirect, url_for,flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin, current_user
 from flask_migrate import Migrate
+import time
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'bet kokia simbolių eilutė'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///L:/baigiamasis_darbas/database_file.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'your_secret_key'  
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+login_manager = LoginManager(app)
+Migrate(app, db) 
 
-class User(db.Model):
-    __tablename__ = 'users'
+class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-
-    def __init__(self, username, email, password):
-        self.username = username
-        self.email = email
-        self.password = password
-
-
-class Order(db.Model):
-    __tablename__ = 'orders'
-    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
     email = db.Column(db.String(50))
-    vardas = db.Column(db.String(50))
-    pavarde = db.Column(db.String(50))
-    address1 = db.Column(db.String(50))
-    address2 = db.Column(db.String(50))
-    city = db.Column(db.String(50))
-    zip_code = db.Column(db.String(50)) 
-    telnr = db.Column(db.String(50))
-    pastabos = db.Column(db.String(50))
+    message = db.Column(db.Text)
+
+    def __init__(self, name, email, message):
+        self.name = name
+        self.email = email
+        self.message = message
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password 
+        self.email = email
+
+    def __repr__(self):
+        return f'{self.name} - {self.email} - {self.password}'
+    
 
 
-@app.route('/', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        email = request.form['email']
-        user = User(username=username, email=email, password=password)
-        db.session.add(user)
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    Paemimo = db.Column(db.String(100))
+    Pristatymo = db.Column(db.String(100))
+    Svoris = db.Column(db.String(100))
+    Tel_nr = db.Column(db.String(100))
+    user_id = db.Column(db.String(100))
+    delivered = db.Column(db.Boolean, default=False)
+
+    def __init__(self, Paemimo, Pristatymo, Svoris,Tel_nr,user_id,delivered):
+        self.Paemimo = Paemimo
+        self.Pristatymo = Pristatymo 
+        self.Svoris = Svoris
+        self.Tel_nr = Tel_nr
+        self.user_id = user_id
+        self.delivered = delivered
+
+class SelectedData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    Paemimo = db.Column(db.String(100))
+    Pristatymo = db.Column(db.String(100))
+    Svoris = db.Column(db.String(100))
+    Tel_nr = db.Column(db.String(100))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    delivered = db.Column(db.Boolean, default=False)
+    user = db.relationship('User', backref='selected_data')
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    message = request.form.get('message')
+
+    new_message = Message(name=name, email=email, message=message)
+    db.session.add(new_message)
+    db.session.commit()
+    time.sleep(3)
+    flash('Žinutė išsiūsta sėkmingai!')
+    return  redirect('/')
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+
+                                                           # cia reikia pakisti sugeneruota df
+
+@app.route('/selected_table_view')
+@login_required
+def selected_table_view():
+    user = current_user
+    data = SelectedData.query.filter_by(user_id=user.id).all()
+
+    return render_template('selected_table.html', data=data)
+
+
+@app.route('/table_view')
+@login_required
+def table_view():
+    # Fetch data from the database
+    data = Data.query.all()
+
+    return render_template('table.html', data=data)
+
+@app.route('/selected', methods=['GET', 'POST'])
+@login_required
+def selected():
+    # Fetch data from the database
+    user = current_user
+    selected_delivered_ids = request.form.getlist('selected')
+    selected_delivered_rows = SelectedData.query.filter(SelectedData.id.in_(selected_delivered_ids)).all()
+
+    if selected_delivered_rows:
+        for row in selected_delivered_rows:
+            row.delivered = True
+            data_row = Data.query.get(row.id)
+            data_row.delivered = True
+
         db.session.commit()
-        return redirect('/register_success')
-    return render_template('register.html')
 
-@app.route('/register_success')
-def success():
-    users = User.query.all()
-    return render_template('register_succes.html', users=users)
+    # Fetch updated data from the database
+    data = SelectedData.query.filter_by(user_id=user.id).all()
+
+    return render_template('selected_table.html', data=data)
 
 
-@app.route('/fill_order', methods=['GET', 'POST'])
-def fill_order():
-    form = OrderForm()
-    if form.validate_on_submit():
-        order = Order(email=form.email.data,
-                      vardas=form.vardas.data,
-                      pavarde=form.pavarde.data,
-                      address1=form.address1.data,
-                      address2=form.address2.data,
-                      city=form.city.data,
-                      zip_code=form.zip_code.data,
-                      telnr=form.telnr.data,
-                      pastabos=form.pastabos.data)
-        # db.create_all()
-        db.session.add(order)
-        db.session.commit()
-        return redirect(url_for('order_list'))
-    return render_template('form.html', form=form)
+@app.route('/update', methods=['POST'])
+def update():
+    selected_ids = request.form.getlist('selected')
+    selected_rows = Data.query.filter(Data.id.in_(selected_ids)).all()
+
+    user = current_user
+    for row in selected_rows:
+        new_row = SelectedData(Paemimo=row.Paemimo, 
+                               Pristatymo=row.Pristatymo, 
+                               Svoris=row.Svoris,
+                               Tel_nr=row.Tel_nr,
+                               user_id=user.id, 
+                               delivered=row.delivered)
+        db.session.add(new_row)
+
+    db.session.commit()
+    return redirect(url_for('table_view'))
 
 
 
-@app.route('/order_list')
-def order_list():
-    orders = Order.query.all()
-    return render_template('order_list.html', orders=orders)
+@app.route('/confirm_delivery', methods=['POST'])
+@login_required
+def confirm_delivery():
+    selected_ids = request.form.getlist('selected')
+    selected_rows = SelectedData.query.filter(SelectedData.id.in_(selected_ids)).all()
+
+    for row in selected_rows:
+        row.delivered = True
+        data_row = Data.query.get(row.id)
+        data_row.delivered = True
+
+    db.session.commit()
+
+    return redirect(url_for('selected_table_view'))
 
 
-# @app.route('/form', methods=['GET', 'POST'])
-# def form():
-#     form = OrderForm()
-#     if form.validate_on_submit():
-#         return render_template('success.html', form=form)
-#     return render_template('form.html', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+
+@app.route('/login_succes')
+def login_succes():
+    return render_template('login_succes.html')
+
 
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = None
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        if username != 'admin' or password != 'admin':
-            error = 'Invalid credentials. Please try again.'
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            login_user(user)
+            return redirect(url_for('login_succes'))
         else:
-            return redirect(url_for('fill_order'))
-    return render_template('login.html', error=error)
-  
+            flash('Invalid username or password.')
+    return render_template('login.html')
+
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            # flash('Username already exists. Please choose a different username.')
+            return redirect(url_for('register'))
+
+        # Create a new user object
+        new_user = User(username=username, password=password, email=email)
+
+        # Add the new user to the database
+        db.session.add(new_user)
+        db.session.commit()
+
+        # flash('Registration successful! Please log in.')
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
+
 
 
 if __name__ == '__main__':
     with app.app_context():
+        print("Creating database tables...")
         db.create_all()
+        print("Database tables created.")
     app.run(debug=True)
